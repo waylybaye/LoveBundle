8725171105170bf3cbf5b6518c69af6fc5031ee5
waylybaye <havelove@gmail.com>
2017-08-14 22:41:03 +0800

    add v2ray config

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ export HA_SS_TLS_DOMAINS="${SS_TLS_DOMAINS//,/ }"
 export HA_SS_HTTP_DOMAINS="${SS_HTTP_DOMAINS//,/ }"
 export HA_SSR_HTTP_DOMAINS="${SSR_HTTP_DOMAINS//,/ }"
 export HA_SSR_TLS_DOMAINS="${SSR_TLS_DOMAINS//,/ }"
-export HA_V2RAY_DOMAINS="${V2RAY_DOMAINS//,/ }" 
+export HA_V2RAY_DOMAINS="${V2RAY_DOMAINS//,/ }"
 cat /etc/love/templates/haproxy.conf | mo > /etc/love/haproxy.conf
+cat /etc/love/templates/v2ray.json | mo > /etc/love/v2ray.json
 
 exec "$@"
