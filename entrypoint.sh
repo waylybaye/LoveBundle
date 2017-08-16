d0d1e5ecca7e8da3b066c5a0ab3a8138e13b8c1c
waylybaye <havelove@gmail.com>
2017-08-16 17:30:57 +0800

    v2ray: use two process

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,43 +1,50 @@
 #!/bin/sh
 
-if [ -f "/srv/certs/${HTTP2_DOMAIN}.crt" ]; then
-  export NGHTTPX_CERT="/srv/certs/${HTTP2_DOMAIN}.crt"
-  export NGHTTPX_KEY="/srv/certs/${HTTP2_DOMAIN}.key"
-else
-  gencert.sh $HTTP2_DOMAIN
-  export NGHTTPX_CERT="${CA_ROOT}/${HTTP2_DOMAIN}.self-signed.crt"
-  export NGHTTPX_KEY="--insecure ${CA_ROOT}/${HTTP2_DOMAIN}.self-signed.key"
+if [ ! -z "$HTTP2_DOMAIN" ]; then
+  if [ -f "/srv/certs/${HTTP2_DOMAIN}.crt" ]; then
+    export NGHTTPX_CERT="/srv/certs/${HTTP2_DOMAIN}.crt"
+    export NGHTTPX_KEY="/srv/certs/${HTTP2_DOMAIN}.key"
+  else
+    echo "Generating ${HTTP2_DOMAIN} self-signed cert ..."
+    gencert.sh $HTTP2_DOMAIN
+    export NGHTTPX_CERT="${CA_ROOT}/${HTTP2_DOMAIN}.self-signed.crt"
+    export NGHTTPX_KEY="--insecure ${CA_ROOT}/${HTTP2_DOMAIN}.self-signed.key"
+  fi
 fi
-
-cat /etc/love/templates/supervisord.conf | mo > /etc/love/supervisord.conf
 
 export HA_SS_TLS_DOMAINS="${SS_TLS_DOMAINS//,/ }"
 export HA_SS_HTTP_DOMAINS="${SS_HTTP_DOMAINS//,/ }"
 export HA_SSR_HTTP_DOMAINS="${SSR_HTTP_DOMAINS//,/ }"
 export HA_SSR_TLS_DOMAINS="${SSR_TLS_DOMAINS//,/ }"
-export HA_HTTP2_DOMAIN="${HTTP2_DOMAIN//,/ }"
+
 cat /etc/love/templates/haproxy.conf | mo > /etc/love/haproxy.conf
 
-if [ -f "/srv/certs/${V2RAY_TLS_DOMAIN}.crt" ]; then
-  export V2RAY_TLS_CERT_FILE="/srv/certs/${V2RAY_TLS_DOMAIN}.crt"
-  export V2RAY_TLS_KEY_FILE="/srv/certs/${V2RAY_TLS_DOMAIN}.key"
-else
-  gencert.sh $V2RAY_TLS_DOMAIN
-  export V2RAY_TLS_CERT_FILE="${CA_ROOT}/${V2RAY_TLS_DOMAIN}.self-signed.crt"
-  export V2RAY_TLS_KEY_FILE="${CA_ROOT}/${V2RAY_TLS_DOMAIN}.self-signed.key"
+if [ ! -z "$V2RAY_TLS_DOMAIN" ]; then
+  if [ -f "/srv/certs/${V2RAY_TLS_DOMAIN}.crt" ]; then
+    export V2RAY_TLS_CERT_FILE="/srv/certs/${V2RAY_TLS_DOMAIN}.crt"
+    export V2RAY_TLS_KEY_FILE="/srv/certs/${V2RAY_TLS_DOMAIN}.key"
+  else
+    echo "Generating ${V2RAY_TLS_DOMAIN} self-signed cert ..."
+    gencert.sh $V2RAY_TLS_DOMAIN
+    export V2RAY_TLS_CERT_FILE="${CA_ROOT}/${V2RAY_TLS_DOMAIN}.self-signed.crt"
+    export V2RAY_TLS_KEY_FILE="${CA_ROOT}/${V2RAY_TLS_DOMAIN}.self-signed.key"
+  fi
 fi
 
-if [ -f "/srv/certs/${V2RAY_WS_DOMAIN}.crt" ]; then
-  export V2RAY_WS_CERT_FILE="/srv/certs/${V2RAY_WS_DOMAIN}.crt"
-  export V2RAY_WS_KEY_FILE="/srv/certs/${V2RAY_WS_DOMAIN}.key"
-else
-  gencert.sh $V2RAY_WS_DOMAIN
-  export V2RAY_WS_CERT_FILE="${CA_ROOT}/${V2RAY_WS_DOMAIN}.self-signed.crt"
-  export V2RAY_WS_KEY_FILE="${CA_ROOT}/${V2RAY_WS_DOMAIN}.self-signed.key"
+if [ ! -z "$V2RAY_WS_DOMAIN" ]; then
+  if [ -f "/srv/certs/${V2RAY_WS_DOMAIN}.crt" ]; then
+    export V2RAY_WS_CERT_FILE="/srv/certs/${V2RAY_WS_DOMAIN}.crt"
+    export V2RAY_WS_KEY_FILE="/srv/certs/${V2RAY_WS_DOMAIN}.key"
+  else
+    echo "Generating ${V2RAY_WS_DOMAIN} self-signed cert ..."
+    gencert.sh $V2RAY_WS_DOMAIN
+    export V2RAY_WS_CERT_FILE="${CA_ROOT}/${V2RAY_WS_DOMAIN}.self-signed.crt"
+    export V2RAY_WS_KEY_FILE="${CA_ROOT}/${V2RAY_WS_DOMAIN}.self-signed.key"
+  fi
 fi
 
 
-if [ -n "$OCSERV_DOMAIN" ] then;
+if [ ! -z "$OCSERV_DOMAIN" ] then;
   gencert.sh $OCSERV_DOMAIN $LOVE_USERNAME $LOVE_PASSWORD
   export OCSERV_CA_CERT="$CA_ROOT/hyperapp-ca-key.pem"
 
@@ -50,11 +57,12 @@ if [ -n "$OCSERV_DOMAIN" ] then;
   fi
 fi
 
-cat /etc/love/templates/v2ray.json | mo > /etc/love/v2ray.json
+cat /etc/love/templates/v2ray_tls.json | mo > /etc/love/v2ray_tls.json
+cat /etc/love/templates/v2ray_ws.json | mo > /etc/love/v2ray_ws.json
 cat /etc/love/templates/squid.conf | mo > /etc/love/squid.conf
 cat /etc/love/templates/ocserv.conf | mo > /etc/love/ocserv.conf
 
-if [ -n "$ENABLE_OCSERV" ]; then
+if [ ! -z "$OCSERV_DOMAIN" ]; then
   echo "create ocserv accounts ..."
   echo "${LOVE_PASSWORD}" | ocpasswd -c /etc/love/ocpasswd "${LOVE_USERNAME}"
 
@@ -69,7 +77,7 @@ if [ -n "$ENABLE_OCSERV" ]; then
 fi
 
 
-if [ -n "$ENABLE_HTTP2" ] && [ -n "$HTTP2_DOMAIN" ];then
+if [ ! -z "$HTTP2_DOMAIN" ]; then
   htpasswd -bc /etc/love/passwd "${LOVE_USERNAME}" "${LOVE_PASSWORD}"
   CHOWN=$(/usr/bin/which chown)
   SQUID=$(/usr/bin/which squid)
@@ -87,4 +95,5 @@ if [ -n "$ENABLE_HTTP2" ] && [ -n "$HTTP2_DOMAIN" ];then
   fi
 fi
 
+cat /etc/love/templates/supervisord.conf | mo > /etc/love/supervisord.conf
 exec supervisord --nodaemon --configuration /etc/love/supervisord.conf "$@"
